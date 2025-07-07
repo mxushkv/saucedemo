fd0f1bd093e9f754f51b0c64b2df290b42c96215
mxushkv <mxushkv@gmail.com>
2025-07-07 14:42:16 +0700

    chore: removve unstable inventory test

--- tests/inventory/inventory.spec.ts
+++ tests/inventory/inventory.spec.ts
@@ -5,11 +5,6 @@ test.beforeEach(async ({ inventoryPage }) => {
     await inventoryPage.addItemsToCart([0]);
   });
 
-test('Remove item from cart', async ({ inventoryPage }) => {
-  await inventoryPage.removeItemFromCartByIndex(0);
-});
-
 test('Cart badge item indicator change', async ({ inventoryPage }) => {
-  await inventoryPage.addItemsToCart([0]);
   await inventoryPage.expectCartBadgeCount('1');
 })
